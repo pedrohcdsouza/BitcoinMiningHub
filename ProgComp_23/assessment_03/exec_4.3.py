e421ea807cf063c5e3c3e2649fd15a3bb3e855bd
Pedro Henrique <pedrohcsousa8@hotmail.com>
2023-11-04 10:57:38 -0300

    update

--- ProgComp_23/assessment_03/exec_4.3.py
+++ ProgComp_23/assessment_03/exec_4.3.py
@@ -2,9 +2,13 @@
 
 var = str(input("Write something: "))
 
-posicao = 1
-
-while posicao <= len(var):
-    print(var[0:posicao])
-    posicao += 1
-    
+pos = 0
+j = 1
+while pos >= 0:
+    print(var[0:pos+1])
+    pos += j
+    if pos >= len(var):
+        j = -1
+        pos -= 2
+    elif pos < 0:
+        break
