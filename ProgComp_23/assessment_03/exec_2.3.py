da77ce81d50ad2a7c8e3027203068c87754ac8fc
Pedro Henrique <pedrohcsousa8@hotmail.com>
2023-11-04 10:39:38 -0300

    update

--- ProgComp_23/assessment_03/exec_2.3.py
+++ ProgComp_23/assessment_03/exec_2.3.py
@@ -1,4 +1,6 @@
 #pedrohcdsouza arquive
 
-str = str(input("Write someting: "))
-print(str[0:4])
+text = str(input("Vigenère CIPHER\nWrite your text: "))
+keyword = str(input("Now, write your keyword: "))
+repeated_keyword = keyword * (len(text) // len(keyword))
+print(repeated_keyword)
