3811a2aab7a92a25755ef7b0c83998624f7660c0
Pedro Henrique Cardoso de Souza <20232014050002@ifrn.local>
2023-12-14 20:26:50 -0300

    14.12.23

--- ProgComp_23/assessment_05/exec_2.5.py
+++ ProgComp_23/assessment_05/exec_2.5.py
@@ -1,6 +1,5 @@
-import requests, datetime
+import requests, datetime, os
 strURL = 'https://api.cartolafc.globo.com/atletas/mercado'
-dictCartola = requests.get(strURL, verify=False).json()
 
 # ## Solicite ao usuário o ano desejado (o ano informado não pode ser superior ao ano atual, 
 # para isso deve-se usar as funções/métodos da biblioteca DATETIME para obter/validar o ano atual;
@@ -8,11 +7,13 @@ dictCartola = requests.get(strURL, verify=False).json()
 current_year = datetime.datetime.now().year
 
 #verificando se o ano é maior que o ano atual!
-wanted_year = int(input("Write a year: "))
-while wanted_year > current_year or wanted_year < 2021:
-    wanted_year = int(input(f"O ano desejado não pode sair maior que {current_year} e menor que 2021.\n"))
+while True:
+    wanted_year = int(input(f"Escolha um ano de 2021 à {current_year}\n"))
+    if wanted_year <= current_year and wanted_year > 2021:
+        break
 #
 if wanted_year == current_year:
-    print("algo tem que acontecer")
+    dictCartola = requests.get(strURL, verify=False).json()
 else:
-    print("algo tem que acontecer e fodase")
+    
+    
