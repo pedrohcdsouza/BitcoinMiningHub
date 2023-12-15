43806759572d06e0b2c4b7f9e1a0ad724e4a51ac
Pedro Henrique Cardoso de Souza <20232014050002@ifrn.local>
2023-12-15 19:55:03 -0300

    15.12.2023

--- ProgComp_23/assessment_05/exec_2.5.py
+++ ProgComp_23/assessment_05/exec_2.5.py
@@ -1,19 +1,36 @@
 import requests, datetime, os
 strURL = 'https://api.cartolafc.globo.com/atletas/mercado'
 
-# ## Solicite ao usuário o ano desejado (o ano informado não pode ser superior ao ano atual, 
-# para isso deve-se usar as funções/métodos da biblioteca DATETIME para obter/validar o ano atual;
+strDiretorio = os.path.abspath(__file__)
+strDiretorio = os.path.dirname(strDiretorio)
 
 current_year = datetime.datetime.now().year
 
-#verificando se o ano é maior que o ano atual!
+# ## Verificando se o ano é maior/menor que o necessário.
 while True:
-    wanted_year = int(input(f"Escolha um ano de 2021 à {current_year}\n"))
-    if wanted_year <= current_year and wanted_year > 2021:
-        break
+    try:
+        wanted_year = int(input(f"Escolha um ano de 2021 à {current_year}\n"))
+        if wanted_year <= current_year and wanted_year >= 2021:
+            break
+    except :
+        print("")
 #
+# ## Fazendo a leitura dos arquivos
 if wanted_year == current_year:
     dictCartola = requests.get(strURL, verify=False).json()
 else:
+    strNomeArq = strDiretorio + f'\\cartola_fc_{wanted_year}.json'
+
+    dictOpen = open(strNomeArq,'r',encoding='UTF-8')
+    dictCartola = dictOpen.read()
+    dictOpen.close()
+#
+esquemastaticos = ('343','352','433','442','451','532','541')
+
+
+
+
+
+
     
     
