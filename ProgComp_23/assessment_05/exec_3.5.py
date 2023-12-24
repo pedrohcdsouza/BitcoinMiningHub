73520ebb74482d6e5b390aabe63011f1d51e4be2
Pedro Henrique <pedrohcsousa8@hotmail.com>
2023-12-24 12:36:17 -0300

    24.12.23

--- ProgComp_23/assessment_05/exec_3.5.py
+++ ProgComp_23/assessment_05/exec_3.5.py
@@ -6,32 +6,47 @@ strDiretorio = os.path.dirname(strDiretorio)
 
 # ## Abrindo o arquivo
 with open(strDiretorio + '\\relacao_servidores_ifrn.csv','r',encoding='utf-8') as arqLeitura:
-    strCabecalho = arqLeitura.readline()[:-1]
     lstComponentes = list()
     while True:
+        remCabecalho = arqLeitura.readline()[:-1]
         strLinha = arqLeitura.readline()[:-1]
         if not strLinha: break
         strLinha = strLinha.split(';')
         lstComponentes.append(strLinha)
 #
 
-# ## Separando dados
-siglas = []
-for sigla in lstComponentes:
-    if sigla[11] not in str(siglas):
-        siglas.append([sigla[11]])
-
-#
-
-# ## Montando a lista A
-lista_a = siglas
+# ## Separando dados da Lista A
+siglas = {}
 for tipo in lstComponentes:
-    if tipo[1] not in lista_a[siglas.index(tipo[11])]:
-        lista_a[siglas.index(tipo[11])].append(tipo[1])
+    if tipo[11] not in siglas:
+        siglas[tipo[11]] = [tipo[1]]
+    else:
+        if tipo[1] not in siglas[tipo[11]]:
+            siglas[tipo[11]].append(tipo[1])
 
+# Montando e criando arquivo da lista A
+lista_a = [[sigla, len(tipos)] for sigla, tipos in siglas.items()]
+print(lista_a)
 
-    
-    
+with open(strDiretorio + '\\servidores_campi.csv','w',encoding='utf-8') as arqEscrita:
+    strCabecalho = ["campus", "tipos_de_servidores"]
+    lista_a.insert(0,strCabecalho)
+
+    for linha in lista_a:
+        strLinha = ';'.join([str(item) for item in linha])
+        arqEscrita.write(f'{strLinha}\n')
+
+# ## Separando dados da Lista B
+docentes = {}
+for disciplina in lstComponentes:
+    if disciplina[0] == 'docente':
+        if disciplina[3] not in docentes:
+            docentes[disciplina[3]] = [disciplina[5]]
+        else:
+            if disciplina[5] not in docentes[disciplina[3]]:
+                docentes[disciplina[3]].append(tipo[5])
+print(docentes)
+
     
     
 
