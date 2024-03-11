b10158ae261ee14dc78f079212aa30fa51dbe7e1
Pedro Henrique <pedrohcsousa8@hotmail.com>
2024-03-11 13:12:23 -0300

    Updating question 1

--- ProgRedes_24_01/assessment_01/question_01/main.py
+++ ProgRedes_24_01/assessment_01/question_01/main.py
@@ -1,6 +1,6 @@
 ###############################################################
 #                                                             #
-#  Author @pedrohcdsouza                                      #
+#  Author: @pedrohcdsouza & @MatheusLuizSoares                #
 #  Date: 10/03/2024                                           #
 #                                                             #
 ###############################################################
@@ -10,21 +10,42 @@ import os, sys
 strDir = os.path.abspath(__file__)
 strDir = os.path.dirname(strDir)
 
-ArcName = str(input("Please enter the desired filename (including the extension): "))
-ArqDir = strDir + f'/{ArcName}'
+while True:
+    try:
+        ArcName = str(input("Please enter the desired filename (including the extension): "))
+        ArcDir = os.path.join(strDir, ArcName)
+        with open(ArcDir,'rb') as Arc_rb:
+            ArcData = Arc_rb.read()
+        Keyword = str(input("Please enter a keyword: "))
+        KeyLen = len(Keyword)
+    except FileNotFoundError:
+        print("\nThe desired file was not found!\nTry again...\n")
+    except:
+        print(f"\nERROR: {sys.exc_info()[0]}")
+        sys.exit()
+    else:
+        break
 
-with open(ArqDir,'r') as Arc_r:
-    ArcData = Arc_r.readlines()
+BytesList = []
+for i, byte in enumerate(ArcData):
+    byte_password = ord(Keyword[i % KeyLen])
+    BytesList.append(byte ^ byte_password)
 
-Keyword = str(input("Please enter a keyword: "))
+while True:
+    try:
+        ArcDest = str(input("Please enter the destination file name: "))
+        ArcDestDir = os.path.join(strDir, ArcDest)
 
-
-
-
-
-
-
-
-
-
-ArcDest = str(input("Please enter the destination file name: "))
+        with open(ArcDestDir, 'wb') as Arc_wb:
+            for byte in BytesList:
+                Arc_wb.write(byte.to_bytes(1, byteorder='big'))
+    except FileNotFoundError:
+        print("\nThe file name is invalid!\n")
+        usercommand = str(input("\nPress 0 -> Exit\nPress 1 -> Return\n"))
+        if usercommand == 0:
+            sys.exit()
+    except:
+        print(f"\nERROR: {sys.exc_info()[0]}")
+        sys.exit()
+    else:
+        break
