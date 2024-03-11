b2d98c9e7c806dcb256fbe3e20c769ae5634f64c
Pedro Henrique Cardoso de Souza <20232014050002@ifrn.local>
2024-03-11 19:05:44 -0300

    Adding comments at question 01

--- ProgRedes_24_01/assessment_01/question_01/main.py
+++ ProgRedes_24_01/assessment_01/question_01/main.py
@@ -5,12 +5,13 @@
 #                                                             #
 ###############################################################
 
-import os, sys
+# Verificando o diretório do programa.
+import os, sys                     
+strDir = os.path.abspath(__file__)  
+strDir = os.path.dirname(strDir)   
 
-strDir = os.path.abspath(__file__)
-strDir = os.path.dirname(strDir)
-
-while True:
+# Criando um laço de repetição para solicitar o nome do arquivo (e abrindo-o), e a palavra-chave desejada. Tratando as exceções caso necessário.
+while True: 
     try:
         ArcName = str(input("Please enter the desired filename (including the extension): "))
         ArcDir = os.path.join(strDir, ArcName)
@@ -26,11 +27,13 @@ while True:
     else:
         break
 
+# Verificando a posição atual byte do texto, e do byte da palavra-chave. E utilizando o operador xor (^).
 BytesList = []
 for i, byte in enumerate(ArcData):
     byte_password = ord(Keyword[i % KeyLen])
     BytesList.append(byte ^ byte_password)
 
+# Criando um laço de repetição para solicitar o nome do arquivo (e cria-lo), escrevendo no arquivo cada byte 'criptografado' que foi colocado dentro da lista (BytesList). Tratando as exceções caso necessário.
 while True:
     try:
         ArcDest = str(input("Please enter the destination file name: "))
