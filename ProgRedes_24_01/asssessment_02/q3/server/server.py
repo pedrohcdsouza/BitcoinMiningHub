4a696b452969d70cdc0ef136bc7e25a70660147c
Pedro Henrique Cardoso de Souza <20232014050002@ifrn.local>
2024-07-22 19:10:06 -0300

    Just correcting #

--- ProgRedes_24_01/asssessment_02/q3/server/server.py
+++ ProgRedes_24_01/asssessment_02/q3/server/server.py
@@ -3,19 +3,19 @@ import socket
 HOST = ''
 PORT = 50007
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: # AF_INET = Ipv4 and SOCK_STREAM = TCP
-        print('Server hearing at')
-        server.bind((HOST, PORT)) # Defines HOST (any) and PORT
-        server.listen(1) # Defines simultaneous connections
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: # AF_INET = Ipv4 and SOCK_STREAM = TCP
+    print('Server hearing at')
+    server.bind((HOST, PORT)) # Defines HOST (any) and PORT
+    server.listen(1) # Defines simultaneous connections
+    while True:
         conn, addr = server.accept() # Accept request
         with conn:
             print('Connected by: ', addr)
             while True:
                 data = conn.recv(512) # Buffer 512
                 if not data: break
-                
-                
+            
+            
 
 
 
