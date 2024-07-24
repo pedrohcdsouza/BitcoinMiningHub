46cf5f42c2bd666a35fa1efa35fa73064fd1a728
pedrohcdsouza <pedrohcsousa8@hotmail.com>
2024-07-24 12:54:52 -0300

    increasing

--- ProgRedes_24_01/asssessment_02/q3/client/client.py
+++ ProgRedes_24_01/asssessment_02/q3/client/client.py
@@ -1,9 +1,33 @@
 # @pedrohcdsouza & @matheusluizsoares
 
-import socket
+import socket, os
 
 HOST = '192.168.2.245'
 PORT = 50007
+osDir = os.path.abspath(__file__)
+osDir = os.path.dirname(osDir)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect((HOST, PORT))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.connect((HOST, PORT))
+    while True:
+        opt = str(input('Escreva o nome do arquivo: '))
+        len_arcname = str(len(opt))
+        server.sendall(bytes('PUT\n'+len_arcname+'\r\n\r\n')).encode('utf-8')
+        data = server.recv(512).decode('utf-8')
+        arcsize, rest = data.split('\n',1)
+        with open(osDir + 'download' + opt, 'wb') as archive:
+            actsize = len(rest)
+            archive.write(rest)
+            while actsize < arcsize:
+                data = server.recv(512).decode('utf-8')
+                archive.write(data)
+                actsize += 512
+            print('Arquivo gerado.')
+
+
+    
+
+
+
+    
--- ProgRedes_24_01/asssessment_02/q3/server/server.py
+++ ProgRedes_24_01/asssessment_02/q3/server/server.py
@@ -1,21 +1,36 @@
-import socket
+import socket, os
 
 HOST = ''
 PORT = 50007
+osDir = os.path.abspath(__file__)
+osDir = os.path.dirname(osDir)
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: # AF_INET = Ipv4 and SOCK_STREAM = TCP
-        print('Server hearing at')
-        server.bind((HOST, PORT)) # Defines HOST (any) and PORT
-        server.listen(1) # Defines simultaneous connections
-        conn, addr = server.accept() # Accept request
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client: # AF_INET = Ipv4 and SOCK_STREAM = TCP
+    print('Server hearing at')
+    client.bind((HOST, PORT)) # Defines HOST (any) and PORT
+    client.listen(1) # Defines simultaneous connections
+    while True: # Loop to listen to new clients
+        conn, addr = client.accept() # Accept request
         with conn:
             print('Connected by: ', addr)
-            while True:
-                data = conn.recv(512) # Buffer 512
-                if not data: break
-                
-                
+            buffer = 512
+            data = conn.recv(buffer).decode('utf-8')
+            if not data:
+                print('Desconnected by', addr) 
+                continue
+            header, body = data.split('\r\n\r\n', 1)
+            type, rest = header.split('\n', 1)
+
+            if type == 'GET':
+                arcname = rest
+                arcsize = os.path.getsize(osDir + 'arquivos' + arcname)
+                client.sendall(arcsize+'\n').encode('utf-8')
+                actsize = 0
+                with open(osDir + 'arquivos', 'rb') as archive:
+                    while actsize < arcsize:
+                        data = archive.read(512)
+                        actsize += 512
 
 
 
