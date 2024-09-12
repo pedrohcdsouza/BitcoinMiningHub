5955b01b6fbe2b5832298475acf5a1f378c536fc
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2024-09-12 12:45:46 -0300

    Implementing the 'G' function code

--- ProgRedes_24_01/Bitcoin_Mining_Server/Master/masterlib.py
+++ ProgRedes_24_01/Bitcoin_Mining_Server/Master/masterlib.py
@@ -5,6 +5,16 @@ import socket, struct
 connectedAgents = dict() # Defining the Connected Agents
 transactionsDict = dict() # Defining the Transactions List PS: It's a directory
 tNumber = 0 # Transaction Number
+numClient = 0
+
+def hear(bytes, connection): # Function to hear whatever
+
+    try:
+        data = connection.recv(bytes)
+        while len(data) != bytes:
+            data += connection.recv(1)
+    except:
+        connection.close()
 
 def hearTransactions(): # Function to hear user-Transaction
 
@@ -35,26 +45,35 @@ def connectAgents(server): # Function to connect agents
             connection, IP = server.accept() # Connecting the agent
             connectedAgents[f'{IP[0]}'] = ''
 
-            # Hearing the G Protocol
-
-            rawResponse = connection.recv(11) # G + 10 bytes
-            try:
-
-                while len(rawResponse) != 11:
-                    rawResponse += connection.recv(1)
-
-                protocol, name = struct.unpack('c10s', rawResponse)
-
-            except:
-                continue
-
-            if protocol != b'G':
-                continue
-
-            if name != bytes(10): 
-                connectedAgents[f'{agentIP[0]}'] = name
-
+            # Hearing the PROTOCOL-Type
             
+            while True:
+
+                protocol = connection.recv(1)
+                protocol = struct.unpack('c', protocol)
+
+                # PROTOCOL - G
+
+                if protocol == b'G':
+                    
+                    name = hear(10, connection)
+                    if name != bytes(10):
+                        connectedAgents[f'{IP[0]}'] = name
+
+                    if len(transactionsDict) == 0:
+                        response = struct.pack('c', b'W')
+                        connection.sendall(response)
+                        connection.close()
+                    
+                    numTrans = transactionsDict[len(transactionsDict) - 1]
+
+                    
+                    
+
+                    
+
+
+
 
         except: # finish agent line
 
