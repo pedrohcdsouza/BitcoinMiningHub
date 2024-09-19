27aef5881f3d122d12052474fd742ee362619542
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2024-09-19 16:55:32 -0300

    Finishing

--- ProgRedes_24_01/V2_Bitcoin_ServerClient/Agent/mylib.py
+++ ProgRedes_24_01/V2_Bitcoin_ServerClient/Agent/mylib.py
@@ -29,6 +29,8 @@ def checkWork(sock):
 
 def startMining(sock):
 
+    startTime = datetime.datetime.now()
+
     response = sock.recv(13)
     while len(response) != 13:
         response += sock.recv(1)
@@ -43,14 +45,12 @@ def startMining(sock):
     checkwork = True
 
     while checkwork:
-        
         if numA == 1:
             nonce = 0
         else:
-            nonce = numA * winS
-
-        while nonce < (nonce+winS) -1:
-
+            nonce = winS
+        while nonce < winS*numA -1:
+            print(f'SEARCHING TRANS {numT}...\n')
             bValue = struct.pack('!I', nonce)
             hashx = hashlib.sha256(bValue + trans).digest()
             binario = ''.join(format(byte, '08b') for byte in hashx)
@@ -68,7 +68,8 @@ def startMining(sock):
             protocol = sock.recv(1)
 
             if protocol == b'A':
-                print(f'TRANSACTION: {numT} FINDED at {datetime.datetime.now()}\n')
+                nowTime = datetime.datetime.now()
+                print(f'TRANS {numT}: {nonce} FINDED on {nowTime-startTime} \n')
                 _ = sock.recv(2)
                 _ = sock.recv(3)
             elif protocol == b'R':
@@ -98,6 +99,7 @@ def startApplication(sock, name):
         protocol = sock.recv(1)
 
         if protocol == b'W':
+            print('NO TRANSACTION ...\nWAITING ...\n')
             time.sleep(10)
         
         elif protocol == b'Q':
--- ProgRedes_24_01/V2_Bitcoin_ServerClient/Master/mylib.py
+++ ProgRedes_24_01/V2_Bitcoin_ServerClient/Master/mylib.py
@@ -35,14 +35,14 @@ def startBot():
                 if text == '/start':
                     sendMessage(chat_id, 'Bitcoin Server Commands:\n/validtrans\n/pendtrans\n/clients')
                 elif text == '/validtrans':
-                    validTrans = [f'{k}: {v[1]} by {v[0]}\n' for k,v in founded.items()]
+                    validTrans = [f'{k}: {v[1]} by {v[0]}' for k,v in founded.items()]
                     sendMessage(chat_id, f'VALID TRANS:\n{validTrans}')
                 elif text == '/pendtrans':
-                    pendTrans = [f'{k}: {v[1]}\n' for k, v in transactions.items() if k not in founded.keys()]
+                    pendTrans = [f'{k}: {v[1]}' for k, v in transactions.items() if k not in founded.keys()]
                     sendMessage(chat_id, f'PEND TRANS:\n{pendTrans}')
                 elif text == '/clients':
-                    clients = [f'{k}: {v[0]}\n' for k,v in agents]
-                    sendMessage(chat_id, f'CLIENTS:\n{clients}')
+                    clients = [f'{k}: {v[0]}' for k,v in agents]
+                    sendMessage(chat_id, f'CLIENTS:')
                 else:
                     sendMessage(chat_id, 'ERROR: Invalid command ...')
                 offset = update['update_id'] + 1
@@ -57,56 +57,56 @@ def writeTransactions(sock):
 
     while True:
 
-        # try:
+        try:
 
-        commands = str(input('')) 
+            commands = str(input('')) 
 
-        # Handling each command
+            # Handling each command
 
-        if commands == '/newtrans':
+            if commands == '/newtrans':
 
-            transaction = str(input('WRITE THE TRANSACTION:\n'))
-            bits = int(input('WRITE THE BITS TO BE ZERO:\n'))
-            transactions[t] = [bits, transaction]
-            t += 1
-        
-        elif commands == '/validtrans':
-
-            validTrans = [f'{k}: {v[1]} by {v[0]}\n' for k,v in founded.items()]
-            print(f'VALID TRANS: {validTrans}')
-        
-        elif commands == '/pendtrans':
-
-            pendTrans = [f'{k}: {v[1]}\n' for k, v in transactions.items() if k not in founded.keys()]
-            print(f'PEND TRANS:\n{pendTrans}')
-        
-        elif commands == '/clients':
-
-            clients = [f'{k}: {v[0]}\n' for k,v in agents.items()]
-            print(f'CLIENTS:\n{clients}')
-
-        elif commands == '/close':
-
-            response = struct.pack('c', b'Q')
-            for i in agents.items():
-                    allConn = i[1][1] # Getting the "conn" of all Agents
-                    allConn.sendall(response) # Sending protocol Q
-            sys.exit()
+                transaction = str(input('WRITE THE TRANSACTION:\n'))
+                bits = int(input('WRITE THE BITS TO BE ZERO:\n'))
+                transactions[t] = [bits, transaction]
+                t += 1
             
-        #     else:
+            elif commands == '/validtrans':
 
-        #         print('ERROR: Invalid command ...\n')
+                validTrans = [f'{k}: {v[1]} by {v[0]}' for k,v in founded.items()]
+                print(f'VALID TRANS: {validTrans}')
+            
+            elif commands == '/pendtrans':
 
-        # except SystemExit: # Headling the server close function
+                pendTrans = [f'{k}: {v[1]}' for k, v in transactions.items() if k not in founded.keys()]
+                print(f'PEND TRANS:\n{pendTrans}')
+            
+            elif commands == '/clients':
 
-        #     print('THE SERVER WAS CLOSED ...\n')
-        #     break
+                clients = [f'{k}: {v[0]}' for k,v in agents.items()]
+                print(f'CLIENTS:\n{clients}')
 
-        # except Exception as exp:
+            elif commands == '/close':
 
-        #     print('ERROR: Invalid command ...\n')
-        #     print(exp)
-        #     continue
+                response = struct.pack('c', b'Q')
+                for i in agents.items():
+                        allConn = i[1][1] # Getting the "conn" of all Agents
+                        allConn.sendall(response) # Sending protocol Q
+                sys.exit()
+                
+            else:
+
+                print('ERROR: Invalid command ...\n')
+
+        except SystemExit: # Headling the server close function
+
+            print('THE SERVER WAS CLOSED ...\n')
+            break
+
+        except Exception as exp:
+
+            print('ERROR: Invalid command ...\n')
+            print(exp)
+            continue
 
 
 
@@ -177,6 +177,7 @@ def hearAgents(conn, addr):
                     founded[numT] = [(addr,name),nonce]
                     response = struct.pack('!cH', b'A',numT)
                     conn.sendall(response)
+                    print(f'TRANS {numT}: {nonce} by {addr}:{name}')
 
                     response = struct.pack('!cH', b'I',numT)
 
