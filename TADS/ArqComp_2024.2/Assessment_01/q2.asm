cd4cb5b93f9e2299ed28ad8aeded0105df15ff39
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2024-10-11 19:29:40 -0300

    Adding Q2

---
+++ TADS/ArqComp_2024.2/Assessment_01/q2.asm
@@ -0,0 +1,14 @@
+# Q2) Faça um programa para ler um número inteiro e imprimir o quadrado desse número.
+
+.text
+	main:
+		addi $2, $0, 5
+		syscall
+		# Catching the user input
+		
+		mul $4, $2, $2 # Adding the ² of user input on register '4'
+		
+		addi $2, $0, 1
+		syscall
+		# Priting the result
+		
