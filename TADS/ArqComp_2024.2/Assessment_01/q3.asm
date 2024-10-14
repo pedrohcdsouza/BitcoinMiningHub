10fa15cd180db5ebb5925c96778d7b7043b8319f
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2024-10-14 09:29:16 -0300

    Adding end service and correcting comments

--- TADS/ArqComp_2024.2/Assessment_01/q3.asm
+++ TADS/ArqComp_2024.2/Assessment_01/q3.asm
@@ -2,23 +2,27 @@
 
 .text
 	main:
-	
-		addi $2, $0, 5
-		syscall
 		
 		# User input
+		
+		addi $2, $0, 5
+		syscall
 		
 		add $8, $0, $2 # Saving result in register '8'
 		
+		# User input
+		
 		addi $2, $0, 5
 		syscall
 		
-		# User input
-		
 		mul $4, $8, $2 # Multipyling the inputs
+		
+		# Print
 		
 		addi $2, $0, 1
 		syscall
 		
-		# Print
+		# End service
 		
+		addi $2, $0, 10
+		syscall
