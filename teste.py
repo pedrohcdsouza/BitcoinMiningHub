302ab9272d653c5c2a2872a93ef75c796623c748
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2024-10-16 18:52:47 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-value = int(input('Valor: '))
-
-firstCase = value // 100
-bruteValue = value % 100
-secondCase = bruteValue // 10
-thirdCase = bruteValue % 10
-
-print(f'Divisão: \n{firstCase}\n{secondCase}\n{thirdCase}')
