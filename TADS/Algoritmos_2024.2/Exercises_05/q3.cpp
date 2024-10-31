304ef9d627aa687fefcd36875a26bbed4f879bd2
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2024-10-31 11:52:25 -0300

    q3 on Exercises 5

---
+++ TADS/Algoritmos_2024.2/Exercises_05/q3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    int arrayN[10] = {1,2,3,4,5,6,7,8,9}; // INPUT
+    int x = 0;                            // OUTPUT
+
+    int y = 0;
+
+    for (int i; i < 10; i++){
+
+        if ()
+
+    }
+}
