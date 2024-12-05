e1ec25fc64d58ba0bb670362f33ceb3d2f7b301e
Pedro Henrique Cardoso de Souza <pedro.cardoso@academico.ifrn.edu.br>
2024-12-05 10:17:32 -0300

    Adding questions

--- TADS/Algoritmos_2024.2/Codeforces_02/q1.cpp
+++ TADS/Algoritmos_2024.2/Codeforces_02/q1.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
-#include <list>
+#include <iomanip>
 #include <algorithm>
 
-int main(){
+int main() {
+    int N, K;
+    while(std::cin >> N >> K){
 
-    int a[6] = {2,6,4,3,10,1};
-    int K = (6 * (6-1)*(6-2)/6);
-    int b[K];
-    int count = 0;
+        int a[N];
+        for(int i = 0; i < N; i++){
+            std::cin >> a[i];
+        }
+        
 
-    for (int i = 0; i < 6; i++){
-        for(int j = i + 1; j < 6; j++){
-            for(int k = j + 1; k < 6; k++){
-                b[count] = (a[i] + a[j] + a[k])/3;
-                count += 1;
+        int V = (N * (N - 1) * (N - 2)) / 6;
+        float b[V];
+        int count = 0;
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    b[count] = (a[i] + a[j] + a[k]) / 3.0f; 
+                    count += 1;
+                }
             }
         }
-    }
+
+
+        std::sort(b, b + V);
+
+
+        std::cout << std::fixed << std::setprecision(1) << b[V-K] << std::endl;
+
+        }
     
-
-    std::sort(b, b + 20);
-    for (int l = 0; l < K; l++){
-        std::cout << "indice " << l << " : " << b[l] << "\n";
-    }
-
-    std::cout << b[19];
-
     return 0;
 }
