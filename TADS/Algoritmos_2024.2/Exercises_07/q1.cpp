5d98ec9457094589dc96321c61288c55a28b1b9d
Pedro Henrique Cardoso de Souza <pedro.cardoso@academico.ifrn.edu.br>
2024-12-10 08:28:12 -0300

    Adding questions

---
+++ TADS/Algoritmos_2024.2/Exercises_07/q1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+
+int * increase_capacity(int * data, int &capacity, int &size){
+
+    int * new_array = new int[capacity+100];
+
+    for (int i=0 ; i<size ; ++i)
+        new_array[i] = data[i];
+
+    int * old_array = data;
+
+    delete [] old_array;
+
+    capacity = capacity+100;
+
+    return new_array;
+}
+
+int * push_back(int * data, int &capacity, int &size, int value){
+
+    if (size==capacity)
+        data = increase_capacity(data, capacity, size);
+        data[size++]=value;
+
+    return data;
+}
+
+int main(){
+    int *data,size=0,capacity=100,x;
+    auto beg = std::chrono::high_resolution_clock::now();
+    data = new int[capacity];
+    while (std::cin >> x, x!=-1)
+        data = push_back(data, capacity, size, x);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Quantidade de numeros lidos: " << size << std::endl;
+    std::cout << "Tamanho do vetor: " << capacity << std::endl;
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
+    std::cerr << "Tempo de processamento: " << duration.count() << " microseconds(s)"<< std::endl;
+    delete [] data;
+    return 0;
+}
---
+++ TADS/Algoritmos_2024.2/Exercises_07/q2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int removeItem(int userArray[], int size, int removed){
+
+    
+    
+    return 0;
+}
+
+int main(){
+
+    int A[5] = {1, 2, 3, 4, 5};
+    int B = 5;
+    int C = 2;
+
+
+    removeItem(A, B, C);
+
+    return 0;
+}
